c47878c5661aee67617451a5e8c25c261375035c
Todd Davis <tejastcd@yahoo.com>
2019-02-26 14:14:44 -0500

    updated application config to support db connection local and aws

--- secure-bikefit-api/src/main/java/com/tdlabs/secureapi/SecureApiApplication.java
+++ secure-bikefit-api/src/main/java/com/tdlabs/secureapi/SecureApiApplication.java
@@ -1,13 +1,31 @@
 package com.tdlabs.secureapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecureApiApplication {
+public class SecureApiApplication implements CommandLineRunner {
+
+    public static final Logger logger = LoggerFactory.getLogger(SecureApiApplication.class);
+
+    @Value("${application.name}")
+    private String applicationName;
+
+    @Value("${application.environment}")
+    private String applicationEnvironment;
 
     public static void main(String[] args) {
         SpringApplication.run(SecureApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("#######################################");
+        logger.info("##   {} configured to run in {} Environment   ##",  applicationName, applicationEnvironment);
+        logger.info("#######################################");
+    }
 }
--- secure-bikefit-api/src/main/java/com/tdlabs/secureapi/controller/BicycleController.java
+++ secure-bikefit-api/src/main/java/com/tdlabs/secureapi/controller/BicycleController.java
@@ -3,7 +3,6 @@ package com.tdlabs.secureapi.controller;
 import com.tdlabs.secureapi.model.Bicycle;
 import com.tdlabs.secureapi.repository.BicycleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
